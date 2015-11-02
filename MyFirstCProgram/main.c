c91903aa410f37f60267c67dc4bb2655d89c8ceb
willowcheng <e.wingus@Gmail.com>
2015-11-02 15:54:53 -0500

    Initial commit

---
+++ MyFirstCProgram/main.c
@@ -0,0 +1,18 @@
+//
+//  main.c
+//  MyFirstCProgram
+//
+//  Created by turner on 9/26/13.
+//  Copyright (c) 2013 treehouse. All rights reserved.
+//
+
+#include <stdio.h>
+
+int main()
+{
+
+    // insert code here...
+    printf("Hello, World!\n");
+    return 0;
+}
+
