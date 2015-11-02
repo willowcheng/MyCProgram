ed71285f969e09d2ad04a50a408327b0364ddf48
willowcheng <e.wingus@Gmail.com>
2015-11-02 16:03:28 -0500

    Variables

--- MyFirstCProgram/main.c
+++ MyFirstCProgram/main.c
@@ -10,9 +10,17 @@
 
 int main()
 {
+    int days_in_a_week = 7;
+    float cm_to_in = 2.54;
+    
+    char the_w;
+    the_w = 'W';
 
     // insert code here...
-    printf("Hello, World!\n");
+    printf("%d days in a week.\n", days_in_a_week);
+    printf("%f cm per in.\n", cm_to_in);
+    printf("The %c is a cool hotel\n", the_w);
+    
     return 0;
 }
 
